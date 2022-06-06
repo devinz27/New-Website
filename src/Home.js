82b8c8ac1f0cbad04d6cb06f73edea1d186da390
Devin Zhou <devinzhou101@gmail.com>
2022-06-05 20:13:40 -0400

    typewriter

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,24 @@
-import './Home.css'
+import "./Home.css";
+import Typewriter from "typewriter-effect";
 
-function Home(){
-
-    return(
-        <div className='Home'>
-            <div className='home-wrapper'>
-                <h1>DEVIN ZHOU</h1>
-                <span className='line'></span>
-                <div className='title'>
-                
-                </div>
-            </div>
-        </div>
-    )
-
+function Home() {
+  return (
+    <div className="Home">
+      <div className="home-wrapper">
+        <h1>DEVIN ZHOU</h1>
+        <span className="line"></span>
+        {/* <div className="title"> */}
+        <Typewriter
+          options={{
+            strings: ["Hello"],
+            autoStart: true,
+            loop: true,
+          }}
+        ></Typewriter>
+        {/* </div> */}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
