c04a2e401b4dce4618ebda78e88f92422a315bfd
Devin Zhou <devinzhou@Devins-MacBook-Pro.local>
2022-06-11 23:53:37 -0400

    contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,48 @@
-import './Contact.css'
+import "./Contact.css";
 
-function Contact(){
-
-    return (
-        <div className='Contact'></div>
-    )
+function Contact() {
+  return (
+    <div className="Contact">
+      <div className="contact-wrapper">
+        <div className="contact-title">
+          <h2>GET IN TOUCH</h2>
+          <span className="contact-line"></span>
+        </div>
+        <div className="contact-squares">
+          <div className="contact-square">
+            <div className="inner-square">
+              <img
+                src={require("./static/location.png")}
+                alt="location"
+                className="location"
+              />
+              <span>Toronto, Ontario</span>
+            </div>
+          </div>
+          <div className="contact-square">
+            <div className="inner-square">
+              <img
+                src={require("./static/location.png")}
+                alt="location"
+                className="location"
+              />
+              <span>devinzhou101@gmail.com</span>
+            </div>
+          </div>
+          <div className="contact-square">
+            <div className="inner-square">
+              <img
+                src={require("./static/location.png")}
+                alt="location"
+                className="location"
+              />
+              <span>416-819-0365</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
