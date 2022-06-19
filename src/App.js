6eadbd6b2600fc252ac96049e3a74ed89480f2d2
Devin Zhou <devinzhou@Devins-MacBook-Pro.local>
2022-06-19 16:37:44 -0400

    animation

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import Main from "./Main";
 import React, { useState, useEffect } from "react";
 import MobileNav from "./MobileNav";
 import Sidebar from "./Sidebar";
+import { PuffLoader } from "react-spinners";
 
 function App() {
   const [page, setPage] = useState("Home");
   const [selected, setSelected] = useState("Home");
   const [sidebar, setSidebar] = useState("closed");
   const [deviceSize, changeDeviceSize] = useState(window.innerWidth);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const resizeWidth = () => changeDeviceSize(window.innerWidth);
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
 
     window.addEventListener("resize", resizeWidth); // Update the width on resize
 
     return () => window.removeEventListener("resize", resizeWidth);
-  });
+  }, []);
 
   const callBackParent = (value) => {
     setPage(value);
@@ -33,7 +39,11 @@ function App() {
     setSidebar(value);
   };
 
-  return (
+  return loading === true ? (
+    <div className="loader-wrapper">
+      <PuffLoader size={100} color="#000000"></PuffLoader>
+    </div>
+  ) : (
     <div className="App">
       <div className="app-wrapper">
         {deviceSize > 850 ? (
