4c343318f599790b63215c1a7a65311d54cdafc5
Devin Zhou <devinzhou@Devins-MacBook-Pro.local>
2023-06-22 09:52:37 -0400

    fixed dependencies

--- src/Resume.js
+++ src/Resume.js
@@ -7,9 +7,10 @@ function Resume() {
         src={require("./static/resume.pdf#zoom=FitH")}
         width="100%"
         height="100%"
+        title="resume"
       ></iframe>
       This browser does not support PDF! Click Here to view PDF:{" "}
-      <a target="_blank" href={require("./static/resume.pdf")}>
+      <a rel="noreferrer" target="_blank" href={require("./static/resume.pdf")}>
         Resume{" "}
       </a>
     </div>
